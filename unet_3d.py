b156b3196c191ecb15be7821d3125024ec5da0b9
easoplee <easop.lee@duke.edu>
2022-06-18 18:21:12 -0400

    unet update

--- unet_3d.py
+++ unet_3d.py
@@ -22,71 +22,38 @@ class DoubleConv_3d(nn.Module):
         out = self.conv(x)
         return out
 
-class UpConv_3d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, padding=1, stride=2):
-        super().__init__(UpConv_3d, self)
+class UNet(nn.Module):
 
-        self.upconv = nn.Sequential(
-            nn.ConvTranspose3d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, 
-            stride=stride, padding=padding,output_padding=1, bias=True),
-            nn.ReLU(inplace=True)
-        )
-    
-    def forward(self, x):
-        out = self.upconv(x)
-        return out
+    def __init__(self, num_classes):
+        super().__init__()
 
-class UNet_3d(nn.Module):
-    def __init__(self, num_channels):
-        super(UNet_3d, self).__init__()
 
-        #encoder
-        self.down1 = DoubleConv_3d(in_channels=num_channels, out_channels=64)
-        self.maxpool1 = nn.MaxPool3d(kernel_size=2, stride=2)
-        self.down2 = DoubleConv_3d(in_channels=64, out_channels=128)
-        self.maxpool2 = nn.MaxPool3d(kernel_size=2, stride=2)
-        self.down3 = DoubleConv_3d(in_channels=128, out_channels=256)
-        self.maxpool3 = nn.MaxPool3d(kernel_size=2, stride=2)
-        self.down4 = DoubleConv_3d(in_channels=256, out_channels=512)
-        self.maxpool4 = nn.MaxPool3d(kernel_size=2, stride=2)
-        self.down5 = DoubleConv_3d(in_channels=512, out_channels=1024)
+        self.enc1 = DoubleConv_3d(1, 16)
+        self.enc2 = DoubleConv_3d(16, 32)
+        self.enc3 = DoubleConv_3d(32, 64)
+        self.enc4 = DoubleConv_3d(64, 128)
+        self.enc5 = DoubleConv_3d(128, 128)
+        self.pool = nn.MaxPool3d(kernel_size=2, stride=2)
 
-        #decoder
-        self.upconv4 = UpConv_3d(in_channels=1024, out_channels=512)
-        self.up4 = DoubleConv_3d(in_channels=1024, out_channels=512)
-        self.upconv3 = UpConv_3d(in_channels=512, out_channels=256)
-        self.up3 = DoubleConv_3d(in_channels=512, out_channels=256)
-        self.upconv2 = UpConv_3d(in_channels=256, out_channels=128)
-        self.up2 = DoubleConv_3d(in_channels=256, out_channels=128)
-        self.upconv1 = UpConv_3d(in_channels=128, out_channels=64)
-        self.up1 = DoubleConv_3d(in_channels=128, out_channels=64)
+        self.dec4 = DoubleConv_3d(256, 64)
+        self.dec3 = DoubleConv_3d(128, 32)
+        self.dec2 = DoubleConv_3d(64, 16)
+        self.dec1 = DoubleConv_3d(32, 16)
 
-        #final 1x1 conv
-        self.final_layer = nn.Conv3d(in_channels=64, out_channels=num_channels, kernel_size=1, stride=1, padding=0, biase=True)
+        self.conv_final = nn.Conv3d(in_channels=16, out_channels=num_classes, kernel_size=1)
 
     def forward(self, x):
-        #encoder
-        down1 = self.down1(x)
-        skip1 = self.maxpool1(down1)
-        down2 = self.down2(skip1)
-        skip2 = self.maxpool2(down2)
-        down3 = self.down3(skip2)
-        skip3 = self.maxpool3(down3)
-        down4 = self.down4(skip3)
-        skip4 = self.maxpool4(down4)
-        down5 = self.down5(skip4)
 
-        #decoder with skip connections
-        out = torch.cat((self.upconv4(down5), skip4), dim=1)
-        out = self.up4(out)
-        out = torch.cat((self.upconv3(out), skip3), dim=1)
-        out = self.up3(out)
-        out = torch.cat((self.upconv2(out), skip2), dim=1)
-        out = self.up2(out)
-        out = torch.cat((self.upconv1(out), skip1), dim=1)
-        out = self.up1(out)
+        enc1 = self.enc1(x)
+        enc2 = self.enc2(self.pool(enc1))
+        enc3 = self.enc3(self.pool(enc2))
+        enc4 = self.enc4(self.pool(enc3))
+        enc5 = self.enc5(self.pool(enc4))
 
-        #final layer
-        out = self.final_layer(out)
-
+        dec4 = self.dec4(torch.cat((enc4, F.interpolate(enc5, enc4.shape[2:], mode='trilinear')), 1))
+        dec3 = self.dec3(torch.cat((enc3, F.interpolate(dec4, enc3.shape[2:], mode='trilinear')), 1))
+        dec2 = self.dec2(torch.cat((enc2, F.interpolate(dec3, enc2.shape[2:], mode='trilinear')), 1))
+        dec1 = self.dec1(torch.cat((enc1, F.interpolate(dec2, enc1.shape[2:], mode='trilinear')), 1))
+        out = self.conv_final(dec1)
+        out = torch.sigmoid(out)
         return out
