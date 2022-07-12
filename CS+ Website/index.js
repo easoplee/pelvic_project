72a39883aa477fa20f01df2f5d5501d534c9f708
Ashka Shah <ashka.shah@duke.edu>
2022-07-12 13:02:12 -0400

    website update 7/12

---
+++ CS+ Website/index.js
@@ -0,0 +1,12 @@
+function show() {
+ 
+    /* Access image by id and change
+    the display property to block*/
+    if (document.getElementById('pel-image').style.display = "inline") {
+        document.getElementById('pel-image').style.display = "none"; }
+    
+    if (document.getElementById('pel-image').style.display = "none") {
+        document.getElementById('pel-image').style.display = "inline"; }
+    /*document.getElementById('btnID')
+            .style.display = "none";*/
+}
