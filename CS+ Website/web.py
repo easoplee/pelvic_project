ee3f7291f08eaf6cc9cd24c04125751a864e6701
easoplee <easop.lee@duke.edu>
2022-07-13 12:08:55 -0400

    7/13 update

--- CS+ Website/web.py
+++ CS+ Website/web.py
@@ -1,5 +1,6 @@
 from distutils.command.upload import upload
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_navigation import Navigation
 from flask_session import Session
 from backend_test import predict
 import torch
@@ -32,6 +33,8 @@ import plotly.express as px
 # define a variable to hold you app
 server = Flask(__name__)
 
+# initializing Navigations
+
 external_script = ["https://tailwindcss.com/", {"src": "https://cdn.tailwindcss.com"}]
 app = dash.Dash(__name__, external_scripts=external_script, requests_pathname_prefix="/app/")
 
@@ -40,8 +43,8 @@ app.scripts.config.serve_locally = True
 # fig = "empty"
 
 # define your resource endpoints
-@server.route('/', methods=['GET', 'POST'])
-def index_page():
+@server.route('/inputimages', methods=['GET', 'POST'])
+def input_images():
     if request.method == 'POST':
         uploaded_file = request.files['file']
         filestr = np.load(uploaded_file.stream) #depth x width x height
@@ -53,39 +56,26 @@ def index_page():
         global fig
         fig = figure
 
-        # app.layout = html.Div([
-        #     html.Div([
-        #         children = 
-        #             html.H1(children="Interactive Bone Visualization", className=" py-3 text-5xl font-bold text-gray-800"),
-        #             html.P(children="3D Visualization of the selected bone!")],
-        #             html.Div([children=dcc.Graph(figure=fig, style={"width": "100%","height": "100%"}), className="shadow-xl w-full border-3 rounded-sm")], style={"width": "100%","height": "100%",}
-        # )],
-        # style={
-        #                     "width": "68%",
-        #                     "height": "800px",
-        #                     "display": "inline-block",
-        #                     "border": "3px #5c5c5c solid",
-        #                     "padding-top": "5px",
-        #                     "padding-left": "1px",
-        #                     "overflow": "hidden",
-        #                 }]
-        # )
         app.layout = html.Div(
             [html.H1("Interactive Bone Visualization", className=" py-3 text-5xl font-bold text-gray-800", style={"text-align": "center"}),
             dcc.Graph(figure=fig, className=" shadow-xl py-4 px-24 text-5xl bg-[#1d3557] text-white  font-bold text-gray-800", style={"margin-left":"15%", "margin-right":"15%"})]
         )
         return redirect('/app')
 
-        # im = Image.fromarray(np.uint8(pre))
-        # im = im.convert("L")
-        # byte_io = io.BytesIO()
-        # im.save(byte_io, "JPEG")
-        # encoded_img_data = b64encode(byte_io.getvalue())
-
-        # if uploaded_file.filename != '':
-        #     uploaded_file.save(uploaded_file.filename)
     else:
         return render_template("home.html")
+
+@server.route('/')
+def index_page():
+    return render_template('index.html')
+
+@server.route('/home')
+def home_page():
+    return render_template('index.html')
+
+@server.route('/about')
+def about_page():
+    return render_template('about.html')
 
 application = DispatcherMiddleware(
     server,
